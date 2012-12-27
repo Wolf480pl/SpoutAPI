2af93f845908bd65afcee7cab864d127916ef0be
Wolf480pl <wolf480@interia.pl>
2012-12-27 15:20:55 +0100

    Added YamlConfiguration to CommonPolicy
    Signed-off-by: Wolf480pl <wolf480@interia.pl>

--- src/main/java/org/spout/api/plugin/security/CommonPolicy.java
+++ src/main/java/org/spout/api/plugin/security/CommonPolicy.java
@@ -2,6 +2,7 @@ package org.spout.api.plugin.security;
 
 import java.io.File;
 import java.io.FilePermission;
+import java.net.SocketPermission;
 import java.security.AllPermission;
 import java.security.Permission;
 import java.security.PermissionCollection;
@@ -9,18 +10,30 @@ import java.security.Permissions;
 import java.security.Policy;
 import java.security.ProtectionDomain;
 import java.util.Enumeration;
+import java.util.List;
+import java.util.logging.Level;
 
+import org.spout.api.Engine;
+import org.spout.api.exception.ConfigurationException;
 import org.spout.api.plugin.CommonClassLoader;
 import org.spout.api.plugin.Plugin;
+import org.spout.api.util.config.ConfigurationNode;
+import org.spout.api.util.config.yaml.YamlConfiguration;
 
 public class CommonPolicy extends Policy {
-	private ClassLoader spoutClassLoader = this.getClass().getClassLoader();
+	private ClassLoader spoutClassLoader;
 	private PermissionCollection defaultPluginPerms;
+	private YamlConfiguration config;
+	private Engine engine;
 
-	//TODO: Long-ranged: Read plugin perms from YAML config, remove defaultPluginPerms var (replace with YAML section)
-
-	public CommonPolicy(PermissionCollection defaultPluginPerms) {
+	public CommonPolicy(Engine engine, PermissionCollection defaultPluginPerms, File configFile) {
+		this.engine = engine;
+		this.spoutClassLoader = engine.getClass().getClassLoader();
 		this.defaultPluginPerms = defaultPluginPerms;
+		this.config = new YamlConfiguration(configFile);
+		config.setHeader("This is configuration file for client plugin clearances.",
+				"It is not recommended to edit this file manually. Use the launcher GUI instead.");
+		load();
 	}
 
 	@Override
@@ -42,6 +55,13 @@ public class CommonPolicy extends Policy {
 		CommonPermissionCollection perms = new CommonPermissionCollection();
 		perms.addAll(defaultPluginPerms);
 		perms.add(new FilePermission(plugin.getDataFolder().getAbsolutePath() + File.separator + "-", "read,write,delete"));
+
+		ConfigurationNode pluginNode = config.getChild(plugin.getName());
+		if (pluginNode != null) {
+			perms.addAll(parseFilesystem(pluginNode.getChild("filesystem")));
+			perms.addAll(parseNetwork(pluginNode.getChild("network")));
+			perms.addAll(parseBrowser(pluginNode.getChild("browser")));
+		}
 		return perms;
 	}
 
@@ -84,5 +104,70 @@ public class CommonPolicy extends Policy {
 
 	}
 
+	public void load() {
+		try {
+			config.load();
+			config.save();
+		} catch (ConfigurationException e) {
+			engine.getLogger().log(Level.SEVERE, "Error loading clearance configuration!", e);
+		}
+	}
 
+	public void save() {
+		try {
+			config.save();
+		} catch (ConfigurationException e) {
+			engine.getLogger().log(Level.SEVERE, "Error saving clearance configuration!", e);
+		}
+	}
+
+	private PermissionCollection parseFilesystem(ConfigurationNode node) {
+		if (node == null) {
+			return null;
+		}
+		List<?> nodes = node.getList();
+		PermissionCollection collection = null;
+		for (Object o : nodes) {
+			if (o instanceof String) {
+				FilePermission perm = new FilePermission((String) o, "read,write,delete");
+				if (collection == null) {
+					collection = perm.newPermissionCollection();
+				}
+				collection.add(perm);
+			}
+		}
+		return collection;
+	}
+
+	private PermissionCollection parseNetwork(ConfigurationNode node) {
+		if (node == null) {
+			return null;
+		}
+		List<?> nodes = node.getList();
+		PermissionCollection collection = null;
+		for (Object o : nodes) {
+			if (o instanceof String) {
+				SocketPermission perm = new SocketPermission((String) o, "connect,accept");
+				if (collection == null) {
+					collection = perm.newPermissionCollection();
+				}
+				collection.add(perm);
+			}
+		}
+		return collection;
+	}
+
+	private PermissionCollection parseBrowser(ConfigurationNode node) {
+		if (node == null) {
+			return null;
+		}
+		List<?> nodes = node.getList();
+		PermissionCollection collection = null;
+		for (Object o : nodes) {
+			if (o instanceof String) {
+				// TODO: Make some permission class for opening browser.
+			}
+		}
+		return collection;
+	}
 }
